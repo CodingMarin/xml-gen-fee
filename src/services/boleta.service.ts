c386d2699ddd5d0bd53e43387ae0160c01397362
CodingMarin <marinalexander691@gmail.com>
2024-06-17 18:32:54 -0500

    fix: Update path templates

--- src/services/boleta.service.ts
+++ src/services/boleta.service.ts
@@ -9,13 +9,18 @@ export default class BoletaService {
    * @returns invoice
    */
   static render = (ublVersion: String, data: JSON) => {
+    // Construct the absolute path to the template file
     const templatePath = path.join(
       __dirname,
+      "..",
       "templates",
       `boleta${ublVersion}.xml`
     );
+
+    // Read the template file content
     const template = fs.readFileSync(templatePath, "utf8");
 
+    // Render the template with the provided data
     const rendered = Mustache.render(template, data);
 
     return rendered;
